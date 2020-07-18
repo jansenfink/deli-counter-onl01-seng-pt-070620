8d824b73016a91846952e04f0f7ebb247410d7ac
Jansen Fink <jansenfink@charter.net>
2020-07-18 11:51:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,9 @@ end
   
   
 now_serving(array)
-if array.length != 0 
-  puts "Currently serving #{array.}"
+  if array.length > 0 
+    puts "Currently serving #{array.shift()}"
+  else puts "There is nobody waiting to be served!"
+  end
+end
+  
